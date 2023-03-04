1e95462bd267b012d532eacd0d226a521167b663
Zabciol <janek.zaba05@gmail.com>
2023-03-04 21:14:09 +0100

    add ExpenseItem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 
 function App() {
+  const expenses = [
+    { title: 'Birthday present', amount: 39.99, date: new Date(2023, 3, 16) },
+    { title: 'Birthday present', amount: 39.99, date: new Date(2023, 3, 16) },
+    { title: 'Birthday present', amount: 39.99, date: new Date(2023, 3, 16) },
+    { title: 'Birthday present', amount: 39.99, date: new Date(2023, 3, 16) }
+  ];
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
 
-        <a>
+        <h1>
           Let's get started
-        </a>
+        </h1>
       </header>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}>
+      </ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}>
+      </ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}>
+      </ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}>
+      </ExpenseItem>
+
+
+
     </div>
   );
 }
