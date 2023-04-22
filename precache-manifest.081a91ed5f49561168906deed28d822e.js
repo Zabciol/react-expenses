de85c921344ebdeba65528acdb11b83f206f7527
Zabciol <janek.zaba05@gmail.com>
2023-04-22 10:38:59 +0200

    Updates

---
+++ precache-manifest.081a91ed5f49561168906deed28d822e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8b8a07930a2b57372fd",
+    "url": "/react-expenses/static/css/main.d7b52412.chunk.css"
+  },
+  {
+    "revision": "b8b8a07930a2b57372fd",
+    "url": "/react-expenses/static/js/main.b8b8a079.chunk.js"
+  },
+  {
+    "revision": "45c160e1079b7d24a4d1",
+    "url": "/react-expenses/static/js/1.45c160e1.chunk.js"
+  },
+  {
+    "revision": "4d1144568a2475faf71e",
+    "url": "/react-expenses/static/js/runtime~main.4d114456.js"
+  },
+  {
+    "revision": "2541dde2d5f02322fbcbabe30028acb5",
+    "url": "/react-expenses/index.html"
+  }
+];
